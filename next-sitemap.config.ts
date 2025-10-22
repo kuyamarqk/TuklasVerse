ce65c9d5f42364d61e2d7a2a55bafce46fd9ca9a
Raymart Maqueda <kuyamarqk>
2025-10-22 20:07:10 +0800

    feat: Add next-sitemap configuration file

--- next-sitemap.config.ts
+++ next-sitemap.config.ts
@@ -1,17 +1,22 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://www.tuklasverse.com', // Replace with your actual domain
+import type { IConfig, ISitemapField } from 'next-sitemap';
+
+const config: IConfig = {
+  siteUrl: 'https://www.tuklasverse.com',
   generateRobotsTxt: true,
   changefreq: 'weekly',
   priority: 0.8,
   sitemapSize: 5000,
   exclude: ['/404'],
-  transform: async (config, path) => {
-    return {
-      loc: path,
-      changefreq: config.changefreq,
-      priority: config.priority,
-      lastmod: new Date().toISOString(),
-    };
-  },
+
+  transform: async (
+    config: IConfig,
+    path: string
+  ): Promise<ISitemapField> => ({
+    loc: path,
+    changefreq: config.changefreq,
+    priority: config.priority,
+    lastmod: new Date().toISOString(),
+  }),
 };
+
+export default config;
