cbb2633d8a9eee228ee4b88948c7422b6e711f22
Raymart G. Maqueda <67500339+kuyamarqk@users.noreply.github.com>
2025-10-22 20:22:40 +0800

    Update sitemap config to use environment variable for site URL

--- next-sitemap.config.ts
+++ next-sitemap.config.ts
@@ -1,17 +1,12 @@
-import type { IConfig, ISitemapField } from 'next-sitemap';
-
-const config: IConfig = {
-  siteUrl: 'https://www.tuklasverse.com',
+const config = {
+  siteUrl: process.env.SITE_URL || 'https://www.tuklasverse.com',
   generateRobotsTxt: true,
   changefreq: 'weekly',
   priority: 0.8,
   sitemapSize: 5000,
   exclude: ['/404'],
 
-  transform: async (
-    config: IConfig,
-    path: string
-  ): Promise<ISitemapField> => ({
+  transform: async (config, path) => ({
     loc: path,
     changefreq: config.changefreq,
     priority: config.priority,
@@ -19,4 +14,4 @@ const config: IConfig = {
   }),
 };
 
-export default config;
+module.exports = config;
