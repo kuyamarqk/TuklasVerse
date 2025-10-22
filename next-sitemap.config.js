9c7ab7c20e06fd607cad071be0d467411d878d96
Raymart Maqueda <kuyamarqk>
2025-10-22 20:15:58 +0800

    update package.json

---
+++ next-sitemap.config.js
@@ -0,0 +1,19 @@
+// next-sitemap.config.js
+
+const config = {
+  siteUrl: process.env.SITE_URL || 'https://www.tuklasverse.com',
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.8,
+  sitemapSize: 5000,
+  exclude: ['/404'],
+
+  transform: async (config, path) => ({
+    loc: path,
+    changefreq: config.changefreq,
+    priority: config.priority,
+    lastmod: new Date().toISOString(),
+  }),
+};
+
+module.exports = config;
